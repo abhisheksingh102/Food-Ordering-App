e966a03c694b1ee7dca8609555305bc400af6e44
Abhishek Singh <abhishek.singh5@tothenew.com>
2024-08-29 19:22:37 +0530

    introduction to use state hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+  const resList = [
+    {
+      data: {
+        name: "Meghana Foods",
+        cuisine: "Biryani, Sweets, North Indian",
+        rating: "4.4 stars",
+        time: "38 minutes",
+      },
+    },
+    {
+      data: {
+        name: "KFC",
+        cuisine: "Burger, Fast Foods",
+        rating: "3.9 stars",
+        time: "27 minutes",
+      },
+    },
+  ];
+
+  const [topRestaurants, setTopRestaurants] = useState(resList);
+
+  const handleTopRatedRestaurants = () => {
+    const filteredList = topRestaurants.filter(
+      (res) => parseFloat(res.data.rating) > 4
+    );
+    setTopRestaurants(filteredList);
+  };
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => handleTopRatedRestaurants()}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        <RestaurantCard
-          name="Meghana Foods"
-          cuisine="Biryani, Sweets, Noth Indian"
-          rating="4.4 stars"
-          time="38 minutes"
-        />
-        <RestaurantCard
-          name="KFC"
-          cuisine="Burger, Fast Foods"
-          rating="3.9 stars"
-          time="27 minutes"
-        />
+        {topRestaurants.map((restaurant) => (
+          <RestaurantCard resData={restaurant} />
+        ))}
       </div>
     </div>
   );
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-const RestaurantCard = (props) => {
-  console.log(props);
+const RestaurantCard = ({ resData }) => {
+  const { cuisine, name, rating, time } = resData.data;
+
   return (
     <div className="res-card">
       <img
@@ -9,10 +10,10 @@ const RestaurantCard = (props) => {
         alt="res-logo"
         src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/4/15/9de3a721-5e96-41d2-bb3e-3cbeff47fbd3_870899.jpg"
       />
-      <h3>{props.name}</h3>
-      <h4>{props.cuisine}</h4>
-      <h4>{props.rating}</h4>
-      <h4>{props.time}</h4>
+      <h3>{name}</h3>
+      <h4>{cuisine}</h4>
+      <h4>{rating}</h4>
+      <h4>{time}</h4>
     </div>
   );
 };
