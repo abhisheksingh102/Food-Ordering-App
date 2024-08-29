3b4cdc5e32be0c0c5974b3dca56626b090686d99
Abhishek Singh <abhishek.singh5@tothenew.com>
2024-08-29 17:40:33 +0530

    show hardcoded data in card

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,7 +6,18 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard />
+        <RestaurantCard
+          name="Meghana Foods"
+          cuisine="Biryani, Sweets, Noth Indian"
+          rating="4.4 stars"
+          time="38 minutes"
+        />
+        <RestaurantCard
+          name="KFC"
+          cuisine="Burger, Fast Foods"
+          rating="3.9 stars"
+          time="27 minutes"
+        />
       </div>
     </div>
   );
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+  console.log(props);
   return (
     <div className="res-card">
       <img
@@ -8,7 +9,10 @@ const RestaurantCard = () => {
         alt="res-logo"
         src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/4/15/9de3a721-5e96-41d2-bb3e-3cbeff47fbd3_870899.jpg"
       />
-      <h3>Meghana Foods</h3>
+      <h3>{props.name}</h3>
+      <h4>{props.cuisine}</h4>
+      <h4>{props.rating}</h4>
+      <h4>{props.time}</h4>
     </div>
   );
 };
