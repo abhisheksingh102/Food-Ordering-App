d0b79cfe27d75a8c04b4f126272a5c050cce3a0d
Abhishek Singh <abhishek.singh5@tothenew.com>
2024-09-06 18:08:44 +0530

    introduction to class based component

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,20 @@
 import React from "react";
+import User from "./User";
+import UserClass from "./UserClass";
 
 const About = () => {
   return (
-    <div>
+    <div className="user-card">
       <h1>About Us</h1>
       <h2>This is a food ordering learning app.</h2>
+      {/* <User
+        name={"Abhishek Singh (Function)"}
+        location={"Varanasi (Function)"}
+      /> */}
+      <UserClass
+        name={"Abhishek Singh (Class)"}
+        location={"Varanasi (Class)"}
+      />
     </div>
   );
 };
